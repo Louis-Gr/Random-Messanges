03cf8f8f47ed6a95d01ccc95b645f8566c713b33
Louis-Gr <80121656+Louis-Gr@users.noreply.github.com>
2021-04-24 12:46:55 +0200

    Function to create Messanges added

--- MessageGenerator.js
+++ MessageGenerator.js
@@ -2,8 +2,65 @@
 
 const subjects = ["Life","The World"]
 const you = "You"
-const verbsThirdPerson = ["is","will","was", "can", "should", "would"]
-const verbsSecondPerson = ["are", "will", "were", "can", "should", "would"]
-const adjectives = ["beautiful","furry","short","small","big", "worth it"]
+const verbsThirdPerson = ["is","will be","was", "can be", "should be", "would be"]
+const verbsSecondPerson = ["are", "will be", "were", "can be", "should be", "would be"]
 const negation = "not"
-const comparison = ["like", "be like", ]
+const comparison = "like"
+const adjectives = ["beautiful","furry","short","small","big", "worth it"]
+const comparisonWords = ["Water","the Elements","Jürgen Drews"]
+
+
+// Random Calls
+
+const randomSubject = subjects[Math.floor(Math.random()*subjects.length)]
+const randomVerbsThirdPerson =verbsThirdPerson[Math.floor(Math.random()*verbsThirdPerson.length)]
+const randomVerbsSecondPerson = verbsSecondPerson[Math.floor(Math.random()*verbsSecondPerson.length)]
+const randomAdjectiv = adjectives[Math.floor(Math.random()*adjectives.length)]
+const randomComparisonWord = comparisonWords[Math.floor(Math.random()*comparisonWords.length)]
+
+// Verschiedne Mmöglichkeiten
+
+/* Vergleich mit You
+    Vergleich mit Subject
+    Aussage über You
+    Aussage über Subject
+    */
+   function createScentences(num){
+       for (let i = 0; i < num; i++){
+    const posibility = Math.floor(Math.random()* 4)
+//Posibility 1 = Comparison with "You"
+if (posibility === 0){
+    const possibleNot = Math.floor(Math.random())
+    if (possibleNot === 0)
+    console.log(you + " " + randomVerbsSecondPerson + " " +  comparison + " " + randomComparisonWord + ".")
+    else
+    console.log(you + " " + randomVerbsSecondPerson + " " + negation + " " +  comparison + " " + randomComparisonWord + ".")
+}
+// Posibility 2 = Comparison with "Subject"
+else if (posibility === 1){
+    const possibleNot = Math.floor(Math.random())
+    if (possibleNot === 0)
+    console.log(randomSubject + " " + randomVerbsThirdPerson + " " +  comparison + " " + randomComparisonWord + ".")
+    else
+    console.log(randomSubject + " " + randomVerbsThirdPerson + " " + negation + " " +  comparison + " " + randomComparisonWord + ".")
+}
+// Posibility 3 = Statement with "You"
+else if (posibility === 2){
+    const possibleNot = Math.floor(Math.random())
+    if (possibleNot === 0)
+    console.log(you + " " + randomVerbsSecondPerson + " " + randomAdjectiv + ".")
+    else
+    console.log(you + " " + randomVerbsSecondPerson + " " + negation + " " + randomAdjectiv + ".")
+}
+// Posibility 4 = Statement with "Subject"
+else if (posibility === 4){
+    const possibleNot = Math.floor(Math.random())
+    if (possibleNot === 0)
+    console.log(randomSubject + " " + randomVerbsThirdPerson + " " + randomAdjectiv + ".")
+    else
+    console.log(randomSubject + " " + randomVerbsThirdPerson + " " + negation + " " + randomAdjectiv + ".")
+}
+       }
+   }
+
+   createScentences(5)
